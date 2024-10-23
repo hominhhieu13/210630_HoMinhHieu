4fda7c6b97b8d3af73442105214eaa2e044786e2
Hồ Minh Hiếu <hominhhieu1304@gmail.com>
2024-10-23 10:16:58 +0700

    cau8

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//Khai báo các hàm từ các file khác
+void shellc1();
+void shellc2();
+
+int main() {
+	shellc1(); //gọi hàm từ shellc1.c
+	shellc2(); //gọi hàm từ shellc2.c
+	return 0;
+}
